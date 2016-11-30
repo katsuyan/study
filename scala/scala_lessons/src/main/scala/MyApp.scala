ad70a2cb2cf67e707a379c3026868b448b9aaa73
Katsuya Tajima <zakichan1216@gmail.com>
2016-11-30 19:51:50 +0900

    add

--- scala/scala_lessons/src/main/scala/MyApp.scala
+++ scala/scala_lessons/src/main/scala/MyApp.scala
@@ -1,5 +1,15 @@
 object MyApp {
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    val i = args(0).toInt
+    val result = if (i % 3 == 0 && i % 5 == 0) {
+      "FizzBuzz"
+    } else if(i % 3 == 0) {
+      "Fizz"
+    } else if(i % 5 == 0) {
+      "Buzz"
+    } else {
+      i.toString
+    }
+    println(result)
   }
 }
