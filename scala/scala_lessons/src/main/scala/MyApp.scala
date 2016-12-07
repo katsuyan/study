8a04519fd17a13f83112b44c911253788f526ef1
Katsuya Tajima <zakichan1216@gmail.com>
2016-12-07 11:07:23 +0900

    ..

--- scala/scala_lessons/src/main/scala/MyApp.scala
+++ scala/scala_lessons/src/main/scala/MyApp.scala
@@ -1,15 +1,5 @@
 object MyApp {
   def main(args: Array[String]): Unit = {
-    val i = args(0).toInt
-    val result = if (i % 3 == 0 && i % 5 == 0) {
-      "FizzBuzz"
-    } else if(i % 3 == 0) {
-      "Fizz"
-    } else if(i % 5 == 0) {
-      "Buzz"
-    } else {
-      i.toString
-    }
-    println(result)
+    println(List(1,2,3,4).length)
   }
 }
