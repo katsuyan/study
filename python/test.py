5917fbde2ed4a05f8f8b2c59610562c653d5abb0
Katsuya Tajima <zakichan1216@gmail.com>
2017-01-08 21:38:49 +0900

    ...

--- python/test.py
+++ python/test.py
@@ -1,10 +1,16 @@
-# coding: utf-8
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+def main():
+    fizz_buzz(1, 100)
+
+def fizz_buzz(start, stop):
+    for i in range(start, stop + 1):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    main()
